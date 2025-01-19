51d737f50e2a1e6a3d48e23befa4394f127e8128
mossser$ <kanyamosesm@gmail.com>
2025-01-19 20:34:28 +0000

    fixes manifest issue

--- app/models/job_listing.rb
+++ app/models/job_listing.rb
@@ -1,4 +1,8 @@
 class JobListing < ApplicationRecord
   validates :title, :description, :location, presence: true
   scope :active, -> { where(active: true) }
+
+  def self.recent
+    order(created_at: :desc)
+  end
 end
