28808d8a53b41d01e9ed65824a75abdee02e68eb
mossser$ <kanyamosesm@gmail.com>
2025-03-10 13:11:27 +0000

    blog_post test (#28)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_185211) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_190945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_04_185211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "superadmin"
+    t.string "password_digest"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
--- test/controllers/blog_posts_controller_test.rb
+++ test/controllers/blog_posts_controller_test.rb
@@ -1,13 +1,39 @@
 require "test_helper"
 
 class BlogPostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # Create the necessary fixtures or records
+    @blog_post = blog_posts(:one)
+    @blog_post.update(slug: "first-post") if @blog_post.slug.nil?
+    # @blog_post = BlogPost.create!(
+    #   title: "Test Post",
+    #   slug: "test-post",
+    #   content: "Test content",
+    #   excerpt: "Test excerpt",
+    #   category: "Test",
+    #   author: "Test Author",
+    #   read_time: 5,
+    #   published: true,
+    #   published_at: Time.current,
+    #   tag_id: 1 # Explicitly set to nil to avoid foreign key issues
+    # )
+  end
+
   test "should get index" do
-    get blog_posts_index_url
+    # Just test that the route works and assigns the right variables
+    get blog_posts_path
     assert_response :success
   end
 
-  test "should get show" do
-    get blog_posts_show_url
+  test "should get show for valid slug" do
+    # Test the slug-based lookup works correctly
+    get blog_post_path(@blog_post.slug)
     assert_response :success
+  end
+
+  test "should redirect for invalid slug" do
+    get blog_post_path("invalid-slug")
+    assert_redirected_to blog_posts_path
+    assert_equal "Blog post not found", flash[:alert]
   end
 end
