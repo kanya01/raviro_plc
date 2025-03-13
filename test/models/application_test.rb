5095dd241b1d8f411021c9a4c413c1865dc5068a
mossser$ <kanyamosesm@gmail.com>
2025-03-13 14:40:02 +0000

    Add validations for Inquiry model and enhance Application tests

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @job_listing = job_listings(:one)
+    @application = Application.new(
+      name: "John Doe",
+      email: "john@example.com",
+      job_listing: @job_listing
+    )
+  end
+
+  test "should be valid with required attributes" do
+    assert @application.valid?
+  end
+
+  test "should not be valid without a name" do
+    @application.name = nil
+    assert_not @application.valid?
+    assert_includes @application.errors[:name], "can't be blank"
+  end
+
+  test "should not be valid without an email" do
+    @application.email = nil
+    assert_not @application.valid?
+    assert_includes @application.errors[:email], "can't be blank"
+  end
+
+  test "should not be valid without a job_listing" do
+    @application.job_listing = nil
+    assert_not @application.valid?
+    assert_includes @application.errors[:job_listing], "must exist"
+  end
+
 end
